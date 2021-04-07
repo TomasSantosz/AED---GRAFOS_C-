d6cd8ab672e3b57f72122df57e71f1f353091cbe
TomasSantosz <tomassantos016@gmail.com>
2021-04-07 20:42:28 -0300

    first commit Trabalho de Grafos

---
+++ Editor de Grafos/Editor.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Editor_de_Grafos
+{
+    public partial class Editor : Form
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        #region Botoes de Algoritmo do Menu
+        private void BtParesOrd_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(g.paresOrdenados(), "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtGrafoEuleriano_Click(object sender, EventArgs e)
+        {
+            if (g.isEuleriano())
+                MessageBox.Show("O grafo é Euleriano!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("O grafo não é Euleriano!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtGrafoUnicursal_Click(object sender, EventArgs e)
+        {
+            if (g.isUnicursal())
+                MessageBox.Show("O grafo é Unicursal!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("O grafo não é Unicursal!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void BtBuscaProfundidade_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        #endregion --------------------------------------------------------------------------------------------------
+
+        #region botoes restantes do menu
+
+        private void BtNovo_Click(object sender, EventArgs e)
+        {
+            g.limpar();
+        }
+
+        private void BtAbrir_Click(object sender, EventArgs e)
+        {
+            if (OPFile.ShowDialog() == DialogResult.OK)
+            {
+                g.abrirArquivo(OPFile.FileName);
+                g.Refresh();
+            }
+        }
+
+        private void BtSalvar_Click(object sender, EventArgs e)
+        {
+            if (SVFile.ShowDialog() == DialogResult.OK)
+            {
+                g.SalvarArquivo(SVFile.FileName);
+            }
+        }
+
+        private void BtSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtPeso_Click(object sender, EventArgs e)
+        {
+            if (BtPeso.Checked)
+            {
+                BtPeso.Checked = false;
+                g.setExibirPesos(false);
+
+            }
+            else
+            {
+                BtPeso.Checked = true;
+                g.setExibirPesos(true);
+            }
+            g.Refresh();
+        }
+
+        private void BtPesoAleatorio_Click(object sender, EventArgs e)
+        {
+            if (BtPesoAleatorio.Checked)
+            {
+                BtPesoAleatorio.Checked = false;
+                g.setPesosAleatorios(false);
+            }
+            else
+            {
+                BtPesoAleatorio.Checked = true;
+                g.setPesosAleatorios(true);
+            }
+        }
+
+        private void BtSobre_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Editor de Grafos - 2021/1\n\nDesenvolvido por:\nNatan Macedo de Magalhaes\nVirgilio Borges de Oliveira\n\nAlgoritmos e Estruturas de Dados II\nFaculdade COTEMIG\nSomente para fins didáticos.", "Sobre o Editor de Grafos...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        #endregion --------------------------------------------------------------------------------------------------
+
+        private void completarGrafoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.completarGrafo();
+        }
+
+        private void algoritmosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void profundidadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Vertice v = g.getVerticeMarcado();
+            if (v == null)
+            {
+                MessageBox.Show("Escolha a Raiz da busca", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                g.profundidade(v.getNum());
+            }            
+        }
+
+        private void larguraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Vertice v = g.getVerticeMarcado();
+            if (v == null)
+            {
+                MessageBox.Show("Escolha a Raiz da busca", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                g.largura(v.getNum());
+            }           
+        }
+    }
+}
---
+++ Editor de Grafos/Grafo.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Editor_de_Grafos;
+using System.Drawing;
+
+namespace Editor_de_Grafos
+{
+    public class Grafo : GrafoBase, iGrafo
+    {
+        /* private bool[] visitado; */
+                
+       
+        bool[] visitado = new bool[100];
+        
+        
+        public void AGM(int v)
+        {
+
+        }
+
+        public void caminhoMinimo(int i, int j)
+        {
+
+        }
+
+        public void completarGrafo()
+        {
+            for (int i = 0; i < matAdj.GetLength(0); i++)
+            {
+                for (int j = 0; j < matAdj.GetLength(0); j++)
+                {
+                    if (getAresta(i, j) != null)
+                    {
+                        base.OnClick(i);
+                        if (!drag)
+                            f.clicouVertice(this);
+                    }
+                        
+                }
+            }
+        }
+
+        public bool isEuleriano()
+        {
+            int i;
+            for (i = 0; i < getN(); i++)
+            {
+                if (grau(i) % 2 != 0)
+                    return false;
+
+            }
+            if (getN() != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool isUnicursal()
+        {
+            int grauImpar = 0;
+            for (int i = 0; i < matAdj.GetLength(0); i++)
+            {
+                if (grau(i) % 2 != 0)
+                    grauImpar++;
+            }
+            return (grauImpar == 2);
+        }
+
+        public void largura(int v)
+        {
+            Fila f = new Fila(matAdj.GetLength(0));
+            f.enfileirar(v);
+            visitado[v] = true; // marca V como visitado
+            while (!f.vazia())
+            {
+                v = f.desenfileirar(); // retira o próximo vértice da fila
+                for (int i = 0; i < matAdj.GetLength(0); i++)
+                {
+                    // se I é adjacente a V e I ainda não foi visitado
+                    if (matAdj[v, i] != matAdj[0, 0] && !visitado[i])
+                    {
+                        getAresta(v, i).setCor(Color.Orange);
+                        visitado[i] = true; // marca i como visitado
+                        f.enfileirar(i); // enfileira i
+                    }
+                }
+            }
+        }
+
+        public void numeroCromatico()
+        {
+        }
+
+        public string paresOrdenados()
+        {
+            string msg = "E={";
+            for (int i = 0; i < matAdj.GetLength(0); i++)
+            {
+                for (int j = 0; j < matAdj.GetLength(0); j++)
+                {
+                    if (matAdj[i, j] != matAdj[0, 0])
+                        msg += "(" + i + "," + j + "),";
+                }
+            }
+            msg += "}";
+            return msg;
+        }
+        public void profundidade(int v)
+        {
+            
+            visitado[v] = true;
+            
+            for (int i = 0; i < matAdj.GetLength(0); i++)
+            {
+                if (matAdj[v, i] != matAdj[0, 0] && !visitado[i])
+                {
+                    getAresta(v, i).setCor(Color.Orange);
+                    profundidade(i);
+                }
+            }
+        }
+    }
+}
